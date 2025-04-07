92cd06ec5ab79da4463b282c6199ac40b7196673
José Gabriel Álvarez Claros <arestra12_@hotmail.com.com>
2025-04-07 15:12:52 -0600

    Agrego actualizacion de variables

--- config.py
+++ config.py
@@ -1,10 +1,5 @@
 # config.py
 import os
-from dotenv import load_dotenv
-
-# Cargar las variables de entorno
-load_dotenv()
-
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_REGION = os.getenv('AWS_REGION')
